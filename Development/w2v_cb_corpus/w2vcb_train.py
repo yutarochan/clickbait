851b5b0591c2db0bfbac3c30dc6dec1160372d0f
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2017-09-29 20:14:52 -0400

    (update) Create word embedding test setting with logistic regression

--- Development/w2v_cb_corpus/w2vcb_train.py
+++ Development/w2v_cb_corpus/w2vcb_train.py
@@ -19,15 +19,15 @@ warnings.filterwarnings("ignore")
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 ''' Constants and Parameters '''
-DATA_ROOT = '../../Data/dataset/'       # Root Folder of where Dataset Resides
-MODEL_ROOT = '../../Models/dataset/'    # Root Folder of where Model Resides
+DATA_ROOT = '../../Data/dataset/'             # Root Folder of where Dataset Resides
+MODEL_ROOT = '../../Models/w2v_cb_corpus/'    # Root Folder of where Model Resides
 
 WORKERS = 8
 
 # Word Embedding Hyperparameters
-EPOCHS = 20       # Total Epoch Count
-PER_EPOCH = 20    # Per Epoch Count
-DIM = 300         # Total Dimension of the Word Embedding 
+EPOCHS = 15       # Total Epoch Count
+PER_EPOCH = 15    # Per Epoch Count
+DIM = 100         # Total Dimension of the Word Embedding 
 MIN_COUNT = 3     # Minimum Window Size Count
 
 ''' Import Data '''
@@ -36,6 +36,8 @@ print('Loading Dataset')
 data_load = JSONData(DATA_ROOT+'instances_train.jsonl', DATA_ROOT+'truth_train.jsonl', DATA_ROOT+'instances_test.jsonl')
 train_X = data_load.load_train_X()
 train_Y = data_load.load_train_Y()
+
+print(train_X[0]['targetParagraphs'])
 
 ''' Preprocess Data '''
 def preprocess(text):
@@ -46,7 +48,6 @@ def preprocess(text):
 # Preprocess Headline and Article Dataset
 print('Performing Preprocessing')
 data = map(lambda x: preprocess(x['targetTitle']), train_X)    # Headline Only
-# data = map(lambda x: preprocess, train_X)
 
 ''' Train Embedding Model '''
 print('Training Word Embedding Model')
@@ -54,5 +55,6 @@ model = gensim.models.Word2Vec(data, min_count=MIN_COUNT, workers=WORKERS)
 for i in range(EPOCHS):
     model.train(data, total_examples=len(data), epochs=PER_EPOCH)
 
-print('Test Similarity')
-model.most_similar(positive=[''])
+# Persist Weights
+print('Persisting Weights')
+model.save(MODEL_ROOT+'w2v_cb_headline_'+str(EPOCHS)+'_'+str(PER_EPOCH)+'_DIM-'+str(DIM)+'.bin.gz')
--- Development/w2v_logistic/w2v_logistic.py
+++ Development/w2v_logistic/w2v_logistic.py
@@ -1,14 +1,15 @@
-
 '''
 Word2Vec + Logistic Function
 Author: Yuya Jeremy Ong (yjo5006@psu.edu)
 '''
 from __future__ import print_function
 import sys
+import gensim
 import string
 import numpy as np
 from nltk import word_tokenize
 from sklearn.model_selection import KFold
+from sklearn.linear_model import LogisticRegression
 
 sys.path.append('..')
 from text2num import text2num
@@ -25,6 +26,10 @@ K_FOLD = 10
 SHUFFLE_FOLDS = True
 np.random.seed(9892)                    # Seed Parameter for PRNG
 
+# Model Hyperparameters
+MODEL_DIR = '../../Models/w2v_cb_corpus/w2v_cb_headline_15_15_DIM-100.bin.gz'
+COMB = 'MIN'                            # Word Embedding Combination Method
+
 report = ScoreReport('Baseline Model')  # Automated Score Reporting Utility
 
 ''' Import Data '''
@@ -33,13 +38,16 @@ data_load = JSONData(DATA_ROOT+'instances_train.jsonl', DATA_ROOT+'truth_train.j
 train_X = data_load.load_train_X()
 train_Y = data_load.load_train_Y()
 
+# Load Word Embedding Model
+model = gensim.models.Word2Vec.load(MODEL_DIR)
+
 ''' Preprocess Data '''
 # Build Feature Vector
 def preprocess(text):
     text = word_tokenize(text.lower())                                      # Tokenize & Normalize Text
     text = filter(lambda x: x not in string.punctuation, text)              # Remove Punctuation
     return text
-    
+
 # Finalize Feature and Target Vectors
 X = np.array(map(lambda x: preprocess(x['targetTitle']), train_X))
 Y = np.array(map(lambda x: [0] if x['truthClass'] == 'no-clickbait' else [1], train_Y)
